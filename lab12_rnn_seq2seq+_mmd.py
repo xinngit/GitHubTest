516c7abc8eec09af075aae9b78c5945f34afd23d
xinngit <109780954+xinngit@users.noreply.github.com>
2022-09-28 09:57:45 +1000

    test 2

--- lab12_rnn_seq2seq+_mmd.py
+++ lab12_rnn_seq2seq+_mmd.py
@@ -423,3 +423,5 @@ def translate(sentence, encoder, decoder, inp_lang, targ_lang, max_length_inp, m
 # restoring the latest checkpoint in checkpoint_dir
 checkpoint.restore(tf.train.latest_checkpoint(checkpoint_dir))
 translate('뭐라도 배워볼까.', encoder, decoder, inp_lang, targ_lang, max_length_inp, max_length_targ)
+
+# update for github
